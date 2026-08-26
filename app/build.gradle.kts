6957046653ce312374b3be0e8371794e90eec05d
waxew <4622.ar@gmail.com>
2026-08-26 15:55:48 +0330

    chore(v1.2): bump Android version to 1.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "ir.asteam.telegrambotstore"
         minSdk = 24
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 12
+        versionName = "1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
